3367135fbf2168842e20814a26884d271bc6848a
Amad-Mlinka <amadovski@gmail.com>
2021-04-24 21:46:30 +0200

    Finished first version

--- src/App.js
+++ src/App.js
@@ -6,32 +6,72 @@ const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popula
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=70846ae2c3bdf81734c4dc36fab283cc&query="
 
 
-
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getMovies= (API) => {
+    fetch(API)
+        .then(res => res.json())
+        .then(data => {
+          setMovies(data.results);
+        })
+  }
+
+
+  const onSubmit = (e) => {
+    console.log(searchTerm)
+    e.preventDefault();
+    if (searchTerm && searchTerm.length >= 3) {
+      getMovies(SEARCH_API + searchTerm)
+      setSearchTerm("")
+    }
+  }
+
+  const onChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
 
 
   useEffect(() => {
-    fetch(FEATURED_API)
-      .then(res => res.json())
-      .then(data => {
-        setMovies(data.results);
-      })
-
-
+    getMovies(FEATURED_API)
+    setSearchTerm("")
   }, []);
+
+
+  useEffect(() => {
+    if (searchTerm) {
+      console.log(searchTerm)
+      if (searchTerm.length >= 3) {
+        getMovies(SEARCH_API + searchTerm)
+        setSearchTerm("")
+      }
+      if (searchTerm.length < 3) {
+        getMovies(FEATURED_API)
+        setSearchTerm("")
+      }
+    }
+
+  }, [searchTerm]);
+
 
   return (
     <div className="App">
+      <header>
+        <form action="" className="searchForm" onSubmit={onSubmit} value={searchTerm}>
+          <input className="search" type="search" placeholder="Search" onChange={onChange} />
+        </form>
+
+      </header>
       <div className="movie-container">
-      {
-        movies.length > 0 && movies.map(movie => (
-          <Movie key={movie.id} {...movie} />
-        ))
-      }
+        {
+          movies.length > 0 && movies.map(movie => (
+            <Movie key={movie.id} {...movie} />
+          ))
+        }
       </div>
-      
+
 
     </div>
   );
--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -4,10 +4,9 @@ import "./Movie.scss"
 const IMAGE_API = "https://image.tmdb.org/t/p/w500/"
 
 export const Movie = (movie) => {
-    console.log(movie)
     return (
         <div className="movie">
-            <img src={IMAGE_API + movie.poster_path} alt=""/>  
+            <img src={movie.poster_path!=null ? IMAGE_API + movie.poster_path:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/488px-No-Image-Placeholder.svg.png"} alt=""/>  
             <div className="movie-info">
                 <h3>{movie.title}</h3>
             </div>
